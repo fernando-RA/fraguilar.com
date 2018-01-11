084ba66b93af02945e3e7ce23f4d7c9f87476416
Fernando Aguilar <fernando@aguilar.net.br>
2018-01-11 00:23:15 +0000

    Adding view and url to posts

--- fraguilar/urls.py
+++ fraguilar/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
+from posts import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'admin/', admin.site.urls),
+    path('posts/', views.post_home, name="post_home"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 # How something is displayed inside the page
+def post_home(request):
+    return HttpResponse("<h1>Hello World</h1>")
