42f6bac63a3a738370889b72b93d58578cf3cab1
Fernando <fernando@fraguilar>
2019-06-14 16:58:27 -0300

    Add Blog Posts

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,37 @@ import BlogCard from './../components/blogCard';
 import banner from './../components/images/banner.jpg';
 import github from './../components/images/github.png';
 
+import capa from '../components/images/capa.jpg';
+import blog1 from '../components/images/blog1.jpeg';
+import blog2 from '../components/images/blog2.jpeg';
+import blog3 from '../components/images/blog3.jpg';
 
+const BLOG_ITEMS = [
+  {
+    title: 'How to Know if Someone is Meant to Be in Your Life',
+    author: 'Fernando Aguilar',
+    authorImg: capa,
+    img: blog1,
+    description: 'A short story in two parts.',
+    link: 'https://medium.com/@fraguilar/how-to-know-if-someone-is-meant-to-be-in-your-life-368d02216b25'
+  },
+  {
+    title: 'How To Use The Law of Attraction In Your Life.',
+    author: 'Fernando Aguilar',
+    authorImg: capa,
+    img: blog2,
+    description: 'Manifestation Secrets. The Ascent Publiation',
+    link: 'https://theascent.pub/the-law-of-attraction-how-it-works-2cfbdebcf841'
+  },
+  {
+    title: 'The Greatest Advice for Young People',
+    author: 'Fernando Aguilar',
+    authorImg: capa,
+    img: blog3,
+    description: '10 Things Every 20-Something Needs to Read Right Now.',
+    link: 'https://medium.com/@fraguilar/the-greatest-advice-for-young-people-8061d778abeb'
+  }
+]
 
 
 const About = () => (
@@ -33,8 +63,21 @@ const About = () => (
         </p>
       </div>
     </div>
+    <div className='row'>
+      <div className="col-xs-10 col-xs-offset-1 col-sm-offset-2 Blog">
+        <h1>Recent Blog Posts</h1>
+        {BLOG_ITEMS.map(i => <BlogCard
+          key={i.title}
+          title={i.title}
+          description={i.description}
+          img={i.img}
+          author={i.author}
+          authorImg={i.authorImg}
+          link={i.link}/>)
+        }
+      </div>
+    </div>
   </div>
 )
-
 
 export default About
