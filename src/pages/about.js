8557e391937376bf3268765817e6e62200de642c
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-10-03 15:27:37 -0300

    Update blog posts

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,32 +1,34 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import BlogCard from './../components/blogCard';
+import BlogCard from './../components/blogCard'
 
-import banner from './../components/images/banner.jpg';
-import github from './../components/images/github.png';
+import banner from './../components/images/banner.jpg'
+import github from './../components/images/github.png'
 
-import capa from '../components/images/capa.jpg';
-import blog1 from '../components/images/blog1.jpeg';
-import blog2 from '../components/images/blog2.jpeg';
-import blog3 from '../components/images/blog3.jpg';
+import capa from '../components/images/capa.jpg'
+import blog1 from '../components/images/blog1.jpeg'
+import blog2 from '../components/images/blog2.jpeg'
+import blog3 from '../components/images/blog3.jpg'
 
 const BLOG_ITEMS = [
   {
-    title: 'How to Know if Someone is Meant to Be in Your Life',
+    title: 'How I’m relating Human & Robots in a Greek Tourism Startup.',
     author: 'Fernando Aguilar',
     authorImg: capa,
     img: blog1,
-    description: 'A short story in two parts.',
-    link: 'https://medium.com/@fraguilar/how-to-know-if-someone-is-meant-to-be-in-your-life-368d02216b25'
+    description: 'Chatbots Life Publiation',
+    link:
+      'https://chatbotslife.com/the-most-mind-blowing-experience-ever-39178da8bef8',
   },
   {
     title: 'How To Use The Law of Attraction In Your Life.',
     author: 'Fernando Aguilar',
     authorImg: capa,
     img: blog2,
-    description: 'Manifestation Secrets. The Ascent Publiation',
-    link: 'https://theascent.pub/the-law-of-attraction-how-it-works-2cfbdebcf841'
+    description: 'Holistic Youth Publication',
+    link:
+      'https://medium.com/holistic-youth/who-i-am-how-the-past-got-me-where-i-am-right-now-1ebe1474609d',
   },
   {
     title: 'The Greatest Advice for Young People',
@@ -34,47 +36,53 @@ const BLOG_ITEMS = [
     authorImg: capa,
     img: blog3,
     description: '10 Things Every 20-Something Needs to Read Right Now.',
-    link: 'https://medium.com/@fraguilar/the-greatest-advice-for-young-people-8061d778abeb'
-  }
+    link:
+      'https://medium.com/@fraguilar/the-greatest-advice-for-young-people-8061d778abeb',
+  },
 ]
 
-
 const About = () => (
-  <div className='container-fluid works' id="about">
+  <div className="container-fluid works" id="about">
     <div className="row">
       <div className="col-xs-offset-2 col-xs-8">
-        <img src={banner} alt="banner" className="BannerAbout"/>
+        <img src={banner} alt="banner" className="BannerAbout" />
       </div>
     </div>
 
     <div className="row">
       <div className="col-xs-9 col-sm-7 col-xs-offset-1 col-sm-offset-3 AboutDescription">
         <p>
-          Fernando Ribeiro is focused on becoming his greatest version. Interested  in the intersection between technology, art and psychology he develops software
-          with a higher sense of purpose. His work has been shown internationally across Brazil and Europe,
-          including at the Cultural Complex of Republic, #16 Art, Media Lab/UnB, Rock in Rio, Planetarium Brasilia and more.
+          Fernando Ribeiro is focused on becoming his greatest version.
+          Interested in the intersection between technology, art and psychology
+          he develops software with a higher sense of purpose. His work has been
+          shown internationally across Brazil and Europe, including at the
+          Cultural Complex of Republic, #16 Art, Media Lab/UnB, Rock in Rio,
+          Planetarium Brasilia and more.
         </p>
         <p>fernando.aguilar@hotmail.com.br</p>
         <p>
           <a href="https://twitter.com/fernand0aguilar">@fernand0aguilar</a>
         </p>
-        <p><img src={github} alt="github" className="SocialIcon"/>
+        <p>
+          <img src={github} alt="github" className="SocialIcon" />
           <a href="https://github.com/fernand0aguilar">fernand0aguilar</a>
         </p>
       </div>
     </div>
-    <div className='row'>
+    <div className="row">
       <div className="col-xs-10 col-xs-offset-1 col-sm-offset-2 Blog" id="blog">
         <h1>Recent Blog Posts</h1>
-        {BLOG_ITEMS.map(i => <BlogCard
-          key={i.title}
-          title={i.title}
-          description={i.description}
-          img={i.img}
-          author={i.author}
-          authorImg={i.authorImg}
-          link={i.link}/>)
-        }
+        {BLOG_ITEMS.map(i => (
+          <BlogCard
+            key={i.title}
+            title={i.title}
+            description={i.description}
+            img={i.img}
+            author={i.author}
+            authorImg={i.authorImg}
+            link={i.link}
+          />
+        ))}
       </div>
     </div>
   </div>
